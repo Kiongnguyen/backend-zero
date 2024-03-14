62b7db5a2f8728b7b33e0c93bc78995649ab33d2
Kiongnguyen <65041504+Kiongnguyen@users.noreply.github.com>
2024-03-14 21:48:56 +0300

    #5 add mysql connection vs db

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config(); //dotenv
 const express = require("express"); //common js
 const path = require("path"); //path
+const mysql = require("mysql2"); //sql
 const configViewengine = require("./configs/viewengine");
 const webRoutes = require("./routes/web");
 
@@ -13,6 +14,20 @@ configViewengine(app);
 
 //Routes
 app.use("/", webRoutes);
+
+//test connections
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307, //default: 3306
+  user: "root", //default: empty
+  password: "123456",
+  database: "hoidanit",
+});
+// A simple SELECT query
+connection.query("SELECT * from Users u", function (err, results, fields) {
+  console.log(">>>resaulst>>>", results); // results contains rows returned by server
+  console.log(">>>fields>>>", fields); // fields contains extra meta data about results, if available
+});
 
 //app listen
 app.listen(port, hostname, () => {
